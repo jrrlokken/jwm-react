3e140a4b868183dcfe4836f8d9c4e284715a5ab3
Joshua Lokken <jrrlokken@gmail.com>
2021-03-11 15:13:28 -0600

    Update app to render new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
+import Nav from './Nav';
+import Body from './Body';
+import Footer from './Footer';
+
+import './App.css';
+
 export const App = hot(_App)
 export function _App(): JSXElement | null {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className='App'>
+      <Nav />
+      <Body />
+      <Footer />
     </div>
   );
 }
